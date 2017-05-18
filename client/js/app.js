cea568ca1f884da7344c9d2b1260eb20665b7071
YannickOlivier <yannick.olivier2@gmail.com>
2017-05-19 00:02:30 +0200

    Send profil with Socket.IO

--- client/js/app.js
+++ client/js/app.js
@@ -8,12 +8,15 @@ jQuery(function ($) {
         $('#NameProfil').val(this.value);
     });
 
+
+    // On set VAL '' à l'ID NameProfil
     $('#NewProfil').click(function(el){
         console.log('click');
         $('#NameProfil').val('');
     });
 
-    $('#SaveProfile').click(function() {
+    // On emit via Socket des objects, après click sur updateProfile
+    $('#SaveProfil').click(function() {
         socket.emit('updateProfile', {
           name:$('#NameProfil').val(),
           Format:$('#Format').val(),
